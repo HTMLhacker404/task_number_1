46f46d34f83c58fc5c759d815db27efcdac9ec3d
Aleksey <example@gmail.com>
2020-01-17 13:49:32 +0300

    With sweat and blood

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let money = prompt('Ваш бюджет на месяц?', '');
 let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 
-var appData = {
+let appData = {
 	moneyData : money,
 	timeData : time,
 	expenses : {},
 	optionalExpenses : {},
 	income : [],
 	savings : false
-}
+};
 
 let expenMandOne = prompt('Введите обязательную статью расходов в этом месяце', '');
 let expenPriceOne = prompt('Во сколько обойдется?', '');
@@ -21,8 +21,8 @@ let expenPriceTwo = prompt('Во сколько обойдется?', '');
 
 appData.expenses.expenMandTwo = expenPriceTwo;
 
-let budgetForOneDay = (appData.moneyData - appData.expenses.expenMandOne - appData.expenses.expenMandTwo)/30;
+let budgetForOneDay = (appData.moneyData /30);
 
-alert('Бюджет на один день: ', budgetForOneDay);
+alert(budgetForOneDay);
 
 console.log(budgetForOneDay);
